dcd8e7b5b927643d7178115ee6138caeb7f0e36b
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2022-06-06 22:35:33 +0200

    change model

--- bank_app/models.py
+++ bank_app/models.py
@@ -98,7 +98,7 @@ class Ledger(models.Model):
    def externalTransfer(cls, amount, debit_account, debit_text, credit_account, credit_text, transaction):
       assert amount >= 0
       with transaction.atomic():
-         if debit_account.money >= amount or debit_account.title == "OPS Bank Account":
+         if debit_account.money >= amount:
             # transaction_id = Store.uid
             cls(amount=-amount, transaction=transaction, account=debit_account, text=debit_text,).save()
             cls(amount=amount, transaction=transaction, account=credit_account, text=credit_text).save()
